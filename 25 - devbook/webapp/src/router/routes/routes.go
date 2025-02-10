e31aa9dd4ddaebea48353a2950d8feb5f18a4f50
Thiago Fortunato <tfortuna@visa.com>
2025-02-10 15:55:25 -0300

    formating login page with css

--- 25 - devbook/webapp/src/router/routes/routes.go
+++ 25 - devbook/webapp/src/router/routes/routes.go
@@ -23,5 +23,8 @@ func Build(router *mux.Router) *mux.Router {
 		router.HandleFunc(route.URI, route.Func).Methods(route.Method)
 	}
 
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
+
 	return router
 }
