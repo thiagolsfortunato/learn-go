6ea5277ab58077db656e7cb3078632d9bf36cb9c
Thiago Fortunato <tfortuna@visa.com>
2025-02-10 15:12:25 -0300

    creating admin user

--- 25 - devbook/sql/02-data.sql
+++ 25 - devbook/sql/02-data.sql
@@ -1,4 +1,5 @@
 insert into users (name, nick, email, password) values
+  ("Admin", "admin", "admin@gmail.com", "$2a$10$6/nqDkLec2bgXyXpTGsO..30QaaYCzz9l5FP0A2X88.cpmEX91Sny"), -- admin
   ("User1", "user_1", "user1@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- user1
   ("User2", "user_2", "user2@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- user2
   ("User3", "user_3", "user3@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"); -- user3
